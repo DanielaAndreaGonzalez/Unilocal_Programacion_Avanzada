22d914532c9e04f6080c638c3b889859a075f28d
leidy yuliana rivera <lyriveraq@uqvirtual.edu.co>
2024-04-18 00:38:05 -0500

    se realizan pruebas unitarias para comentario y moderador

--- src/main/java/co/edu/uniquindio/UniLocal/dto/RegistroComentarioDTO.java
+++ src/main/java/co/edu/uniquindio/UniLocal/dto/RegistroComentarioDTO.java
@@ -3,9 +3,7 @@ package co.edu.uniquindio.UniLocal.dto;
 import java.util.Date;
 
 public record RegistroComentarioDTO(
-        String codigoComentario,
         String mensaje,
-        String respuesta,
         String codigoCliente,
         String codigoNegocio,
         Date fechaRegistro,
